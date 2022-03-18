2ab0e4c5f300900328f3a5bda11f254e5df62671
mauriziosodano <maurizio.sodano@gmail.com>
2022-03-18 13:42:10 +0100

    created table

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -6,10 +6,17 @@ import java.sql.SQLException;
 public class App {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
-        String dbUrl="jdbc:sqlite:people.db";
+        String dbUrl = "jdbc:sqlite:people.db";
         var connection = DriverManager.getConnection(dbUrl);
-        System.out.println(connection);
 
+        var stmt = connection.createStatement();
+        var sql = "create table if not exists user (id integer primary key, name text not null)";
+
+        stmt.execute(sql);
+
+        sql="drop table user";
+        stmt.execute(sql);
+        stmt.close();
         connection.close();
 
     }

4519953d4fb7d269f387f16968a65a407e97a807
mauriziosodano <maurizio.sodano@gmail.com>
2022-03-18 13:49:52 +0100

    inserted value in table

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -11,9 +11,22 @@ public class App {
 
         var stmt = connection.createStatement();
         var sql = "create table if not exists user (id integer primary key, name text not null)";
-
         stmt.execute(sql);
 
+        sql="insert into user (id, name) values (0, 'Bob')";
+        stmt.execute(sql);
+
+        sql="insert into user (id, name) values (1, 'Mary')";
+        stmt.execute(sql);
+
+        sql="select id, name from user";
+        var rs=stmt.executeQuery(sql);
+
+        while(rs.next()){
+            int id=rs.getInt("id");
+            String name= rs.getString("name");
+            System.out.println(id + ":"+name);
+        }
         sql="drop table user";
         stmt.execute(sql);
         stmt.close();
